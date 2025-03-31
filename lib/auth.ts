d64e49584d3d6710058084955cf22bcb2e625f72
So-213 <betepp7@gmail.com>
2025-03-31 16:25:30 +0900

    LINE認証完了

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,14 +13,17 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     LINE({
-      clientId: process.env.LINE_CLIENT_ID!,
-      clientSecret: process.env.LINE_CLIENT_SECRET!,
+      clientId: process.env.AUTH_LINE_ID,
+      clientSecret: process.env.AUTH_LINE_SECRET,
+      issuer: 'https://access.line.me',
+      checks: ['pkce', 'state'], // ←ここを追加！
     }),
   ],
   // 任意: セッション戦略・コールバックなど
   session: { strategy: "jwt" },
   trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
+  // debug:true
 })
 
 
